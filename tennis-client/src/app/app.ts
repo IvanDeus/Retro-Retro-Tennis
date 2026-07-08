c3be74f35a7dcf0dd8819525f1991b2e55938fba
IvanDeus <146208093+IvanDeus@users.noreply.github.com>
2026-07-08 09:03:23 +0300

    Update app.ts

--- tennis-client/src/app/app.ts
+++ tennis-client/src/app/app.ts
@@ -1,12 +1,22 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { DiscordSDK } from '@discord/embedded-app-sdk';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  template: `<app-rrt></app-rrt>`
 })
-export class App {
-  protected readonly title = signal('tennis-client');
+export class AppComponent implements OnInit {
+  private discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+
+  async ngOnInit() {
+    // Must authenticate within the Discord Activity Frame environment
+    await this.discordSdk.ready();
+    await this.discordSdk.commands.authorize({
+      client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+      response_type: 'code',
+      state: '',
+      prompt: 'none',
+      scope: ['identify', 'guilds'],
+    });
+  }
 }
